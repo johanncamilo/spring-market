307cb6723ae11f1559d98f1abc3dcc59ce6d99ae
Johann mac <milodevcool@gmail.com>
2024-10-13 10:39:14 -0500

    Challenge Purchase Service & Controller completed

---
+++ src/main/java/com/spring_market/web/controller/PurchaseController.java
@@ -0,0 +1,38 @@
+package com.spring_market.web.controller;
+
+import com.spring_market.domain.Purchase;
+import com.spring_market.domain.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchases")
+public class PurchaseController {
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Purchase>> getAll() {
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/byClient/{id}")
+    public ResponseEntity<List<Purchase>> getByClient(@PathVariable("id") String clientId) {
+        return ResponseEntity.of(purchaseService.getByClient(clientId));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
+    }
+
+}
